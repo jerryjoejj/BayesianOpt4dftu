ea18ea7570dea7dc14121ec2d20333540e92cc4a
lovemanai <jerryjoejj@hotmail.com>
2021-12-05 13:02:45 +0800

    init myself

--- example/1d/example.py
+++ example/1d/example.py
@@ -22,16 +22,17 @@ def parse_argument():
     threshold: Convergence threshold of Bayesian optimization process.
     """
     parser = argparse.ArgumentParser(description='params')
-    parser.add_argument('--which_u', dest='which_u', type=tuple, default=(1,0))
+    parser.add_argument('--which_u', dest='which_u', type=tuple, default=(1, 0))
     parser.add_argument('--br', dest='br', type=int, default=4)
     parser.add_argument('--kappa', dest='kappa', type=float, default=2.5)
     parser.add_argument('--alpha1', dest='alpha1', type=float, default=1)
     parser.add_argument('--alpha2', dest='alpha2', type=float, default=1)
     parser.add_argument('--threshold', dest='threshold', type=float, default=0.001)
-    parser.add_argument('--urange', dest='urange', type=tuple, default=(-10,10))
+    parser.add_argument('--urange', dest='urange', type=tuple, default=(-10, 10))
     parser.add_argument('--import_kpath', dest='import_kpath', type=bool, default=False)
 
     return parser.parse_args()
+
 
 def main():
     args = parse_argument()
@@ -45,26 +46,25 @@ def main():
 
     os.environ['VASP_PP_PATH'] = VASP_PP_PATH
 
-    calculate(command=VASP_RUN_COMMAND, outfilename=OUTFILENAME, method='hse', import_kpath = import_kpath)
-    
+    calculate(command=VASP_RUN_COMMAND, outfilename=OUTFILENAME, method='hse', import_kpath=import_kpath)
+
     if os.path.exists('u.txt'):
         os.remove('u.txt')
 
-    obj = 0 
+    obj = 0
     threshold = args.threshold
     for i in range(50):
-        calculate(command=VASP_RUN_COMMAND, outfilename=OUTFILENAME, method='dftu', import_kpath = import_kpath)
+        calculate(command=VASP_RUN_COMMAND, outfilename=OUTFILENAME, method='dftu', import_kpath=import_kpath)
         db = delta_band(bandrange=br, path='./')
         db.deltaBand()
 
-        bayesianOpt = bayesOpt_DFTU(path='./', kappa=k, alpha_1=a1, alpha_2=a2 )
+        bayesianOpt = bayesOpt_DFTU(path='./', kappa=k, alpha_1=a1, alpha_2=a2)
         obj_next = bayesianOpt.bo(which_u, urange)
         if abs(obj_next - obj) <= threshold:
             print("Optimization has been finished!")
             break
-        obj = obj_next        
+        obj = obj_next
+
 
 if __name__ == "__main__":
     main()
-    
-
